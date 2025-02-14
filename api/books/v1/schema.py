659771f0f3382f33ff4b4b82a9ba68fa821a4f04
EmmyAnieDev <Emaduilzjr1@gmail.com>
2025-02-15 00:30:44 +0100

    docs: Separate CRUD logic using service classes

--- api/books/v1/schema.py
+++ api/books/v1/schema.py
@@ -1,8 +1,22 @@
+import uuid
+from datetime import datetime
+
 from pydantic import BaseModel
 
 
 class BookModel(BaseModel):
-    id: int
+    uid: uuid.UUID
+    title: str
+    author: str
+    publisher: str
+    published_date: str
+    page_count: int
+    language: str
+    created_at: datetime
+    updated_at: datetime
+
+
+class BookCreateModel(BaseModel):
     title: str
     author: str
     publisher: str
---
+++ api/books/v1/service.py
@@ -0,0 +1,54 @@
+from sqlmodel import desc, select
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+from .models import Book
+from .schema import BookCreateModel, BookUpdateModel
+
+
+class BookService:
+    async def get_all_books(self, session: AsyncSession):
+        statement = select(Book).order_by(desc(Book.created_at))
+        result = await session.exec(statement)
+        return result.all()
+
+
+    async def get_book(self, book_uid: str, session: AsyncSession):
+        statement = select(Book).where(Book.uid == book_uid)
+        result = await session.exec(statement)
+        book = result.first()
+        return book if book is not None else None
+
+
+    async def create_a_book(self, book_data: BookCreateModel, session: AsyncSession):
+        book_data_dict = book_data.model_dump()  # converts to a dictionary
+        new_book = Book(**book_data_dict)
+        session.add(new_book)
+        await session.commit()
+        await session.refresh(new_book)
+        return new_book
+
+
+    async def update_book(self, book_uid: str, update_data: BookUpdateModel, session: AsyncSession):
+        book_to_update = await self.get_book(book_uid, session)
+        if book_to_update is None:
+            return None
+
+        update_data_dict = update_data.model_dump(exclude_unset=True)  # Exclude unset fields
+
+        # Iterate through the update_data_dict and update each field of the book instance
+        for k, v in update_data_dict.items():
+            setattr(book_to_update, k, v)  # Dynamically update the book's attributes with new values
+
+        await session.commit()
+        await session.refresh(book_to_update)  # Refresh to get updated data
+        return book_to_update
+
+
+    async def delete_book(self, book_uid: str, session: AsyncSession):
+        book_to_delete = await self.get_book(book_uid, session)
+        if book_to_delete is None:
+            return None
+
+        await session.delete(book_to_delete)
+        await session.commit()
+        return True
